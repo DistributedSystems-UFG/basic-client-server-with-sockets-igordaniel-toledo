cd6392fe61ffffe16a229753390b8e4b417373f7
Igor Toledo <105389445+igordaniel-toledo@users.noreply.github.com>
2026-03-23 15:23:27 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,18 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
-s.close()               # close the connection
+s.connect((HOST, PORT))
+
+while True:
+    msg = input("Digite operação (ex: add 10 5) ou 'sair': ")
+
+    if msg.lower() == "sair":
+        break
+
+    s.send(msg.encode())
+
+    data = s.recv(1024)
+    print("Resposta do servidor:", data.decode())
+
+s.close()
